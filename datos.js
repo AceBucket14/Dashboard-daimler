61fb4283a704482438561d8f35470749b431eb1c
AceBucket14 <brian.andradee1420@gmail.com>
2026-04-09 13:01:37 -0600

    Primer commit - Dashboard Daimler

---
+++ datos.js
@@ -0,0 +1,83 @@
+const datosDaimler = {
+    "kpis": {
+        "total_promesas_mes": 494,
+        "cantidad_gestiones_ayer": 4854,
+        "efectividad_ayer": 1.46,
+        "tiempo_muerto_pct": 46.8,
+        "promedio_por_asesor": 202,
+        "rpc_pct": 3.3
+    },
+    "premios": {
+        "efectividad": "KALED SOSA",
+        "trabajador": "DIANA PAOLA LICEA",
+        "racha": "KALED SOSA"
+    },
+    "carrera": {
+        "nombres": [
+            "KALED SOSA",
+            "AURORA MARTINEZ",
+            "CELESTE ABIGAIL PUENTE",
+            "INGRID LIZETH MUCIO",
+            "CARIM JAZIEL GAUNA",
+            "JAZMIN CARRIZALES",
+            "JESUS ANDRADE",
+            "GERARDO PARRA",
+            "MAURICIO AGUIRRE",
+            "JETRO ABDIEL HERNANDEZ"
+        ],
+        "cantidades": [
+            60,
+            45,
+            42,
+            41,
+            39,
+            31,
+            24,
+            21,
+            21,
+            21
+        ]
+    },
+    "grafica_hora": {
+        "horas": [
+            "8h",
+            "9h",
+            "10h",
+            "11h",
+            "12h",
+            "13h",
+            "14h",
+            "15h",
+            "16h",
+            "17h",
+            "18h",
+            "19h",
+            "20h"
+        ],
+        "conteo": [
+            725,
+            864,
+            771,
+            463,
+            505,
+            493,
+            655,
+            124,
+            93,
+            39,
+            22,
+            18,
+            25
+        ]
+    },
+    "contactability": {
+        "CLIENTE ABANDONA LA LLAMADA": 75,
+        "PROMESA DE PAGO": 71,
+        "TELÉFONO EQUIVOCADO": 41,
+        "DICE QUE YA PAGÓ": 38,
+        "NEGATIVA DE PAGO": 29,
+        "ACLARACIÓN": 20,
+        "RECADO CON FAMILIAR": 15,
+        "DISMINUCIÓN DE INGRESOS": 12
+    }
+};
