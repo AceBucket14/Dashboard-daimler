dddc6f6c126178086f19931ffc7c681278c1eb13
AceBucket14 <brian.andradee1420@gmail.com>
2026-04-27 13:44:01 -0600

    Triple Update: 13:44

--- datos_ayer.js
+++ datos_ayer.js
@@ -1,19 +1,41 @@
 const datosDaimler = {
     "periodo": "DIARIO (AYER)",
     "kpis": {
-        "total_promesas": 0,
-        "total_gestiones": 0,
-        "promedio_por_asesor": 0,
-        "rpc_pct": 0
+        "total_promesas": 78,
+        "total_gestiones": 4453,
+        "promedio_por_asesor": 202,
+        "rpc_pct": 3.0
     },
     "premios": {
-        "efectividad": "---",
-        "trabajador": "---",
-        "racha": "---"
+        "efectividad": "ROSA NIDIA GUZMAN",
+        "trabajador": "NATALIA JARETZI SALINAS",
+        "racha": "GAEL ZAID LABRA"
     },
     "carrera": {
-        "nombres": [],
-        "cantidades": []
+        "nombres": [
+            "GAEL ZAID LABRA",
+            "ROSA NIDIA GUZMAN",
+            "BRAYAN YAHIR HERNANDEZ",
+            "LYDIA IVETH LOZANO",
+            "NATALIA JARETZI SALINAS",
+            "CARIM JAZIEL GAUNA",
+            "MARIA DE LOURDES FLORES",
+            "INGRID LIZETH MUCIO",
+            "JAZMIN CARRIZALES",
+            "DIANA PAOLA LICEA"
+        ],
+        "cantidades": [
+            8,
+            7,
+            7,
+            7,
+            6,
+            6,
+            6,
+            4,
+            4,
+            4
+        ]
     },
     "grafica_hora": {
         "horas": [
@@ -32,20 +54,29 @@ const datosDaimler = {
             "20h"
         ],
         "conteo": [
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0
+            705,
+            657,
+            695,
+            518,
+            352,
+            508,
+            307,
+            88,
+            94,
+            43,
+            132,
+            183,
+            158
         ]
     },
-    "contactability": {}
+    "contactability": {
+        "CLIENTE ABANDONA LA LLAMADA": 79,
+        "PROMESA DE PAGO": 73,
+        "DICE QUE YA PAGÓ": 34,
+        "TELÉFONO EQUIVOCADO": 19,
+        "ACLARACIÓN": 19,
+        "RECADO CON FAMILIAR": 13,
+        "NEGATIVA DE PAGO": 9,
+        "MENSAJE CON TERCEROS": 8
+    }
 };
